e895cdf8453a012e1afd9a9f94dd2c32531a7d09
Olivia <zhaojingyi0430@163.com>
2018-04-07 15:32:13 +0800

    high_concurrency learning

---
+++ src/hc_01/p3/T.java
@@ -0,0 +1,14 @@
+package hc_01.p3;
+
+/**
+ * Created by ZJYYY on 2018/4/6.
+ */
+public class T {
+
+    private int count = 10;
+
+    public synchronized void m() {//等同于在方法的代码执行时要synchronized(this)
+        count--;
+        System.out.println(Thread.currentThread().getName() + " count = " + count);
+    }
+}
